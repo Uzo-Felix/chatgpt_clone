819e7847957a9449e42c8e1147e38969e7175d5d
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-03-13 07:50:46 +0100

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Navbar, Homepage } from './container';
+import './App.css';
+
+const App = () => (
+    <div className="app">
+      <Navbar />
+      <Homepage />
+  </div>
+);
+
+export default App;
